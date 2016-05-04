05a19a45e7b0d7779defc97f597f5c061570f19a
benser1 <briganenser28@gmail.com>
2016-05-04 13:45:45 -0400

    Linked workout to user

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :exercises
   resources :workouts
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:show]
 
   root "workouts#index"
   # The priority is based upon order of creation: first created -> highest priority.
